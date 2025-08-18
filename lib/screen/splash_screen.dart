f5fe551affeb0c4edda9432bb47c68a28cb029ef
Amalia Swastika Indra Prasetya <102946499+AmaliaSwastika@users.noreply.github.com>
2025-08-19 01:26:33 +0700

    Add Check Session Login

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'login_screen.dart';
+import 'navbar/navbar.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,14 +15,31 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    _checkLogin();
+  }
 
-    // Timer 3 detik → pindah ke LoginScreen
-    Timer(const Duration(seconds: 3), () {
+  Future<void> _checkLogin() async {
+    // Timer 3 detik untuk splash
+    await Future.delayed(const Duration(seconds: 3));
+
+    final prefs = await SharedPreferences.getInstance();
+    final savedUsername = prefs.getString('username');
+
+    if (savedUsername != null) {
+      // User sudah login → langsung ke NavBar
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => NavBar(username: savedUsername)),
+      );
+    } else {
+      // User belum login → ke LoginScreen
+      if (!mounted) return;
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (_) => const LoginScreen()),
       );
-    });
+    }
   }
 
   @override
@@ -31,8 +50,6 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Icon(Icons.store, size: 100, color: Colors.white),
-            // SizedBox(height: 20),
             Text(
               "Panda Biru",
               style: TextStyle(
@@ -42,7 +59,6 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
             SizedBox(height: 10),
-            // CircularProgressIndicator(color: Colors.white),
           ],
         ),
       ),
