f0d5b6b643a23dea2aebce1661ebc5b2406eeaa7
Amalia Swastika Indra Prasetya <102946499+AmaliaSwastika@users.noreply.github.com>
2025-08-19 01:26:55 +0700

    Add Logout and Remove Token

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:panda_biru/screen/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String? _username;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUsername();
+  }
+
+  Future<void> _loadUsername() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _username = prefs.getString('username');
+    });
+  }
+
+  Future<void> _logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('username');
+    await prefs.remove('token');
+
+    // Navigasi ke LoginScreen dan hilangkan semua screen sebelumnya
+    if (!mounted) return;
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginScreen()),
+      (route) => false,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("ProfileScreen")),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_username != null)
+              Text(
+                "Halo, $_username!",
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _logout,
+              child: const Text("Logout"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
