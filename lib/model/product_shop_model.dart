094eadd1bb146324789920c2e1dae3024f002705
Amalia Swastika Indra Prasetya <102946499+AmaliaSwastika@users.noreply.github.com>
2025-08-19 03:01:34 +0700

    Update

---
+++ lib/model/product_shop_model.dart
@@ -0,0 +1,19 @@
+class ProductModel {
+  final String product;
+  final String barcode;
+  bool available; // bisa diubah saat ceklis
+
+  ProductModel({
+    required this.product,
+    required this.barcode,
+    this.available = false,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "product": product,
+      "barcode": barcode,
+      "available": available,
+    };
+  }
+}
