4e192395019703af7ef07a884b11c737215235dc
Amalia Swastika Indra Prasetya <102946499+AmaliaSwastika@users.noreply.github.com>
2025-08-19 00:51:41 +0700

    Add Theme Color

---
+++ lib/theme/theme_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  Color whiteColor = const Color(0xFFFFFFFF);
+  Color blackColor = const Color(0xFF000000);
+  Color blueColor = const Color(0xFF295F98);
+}
