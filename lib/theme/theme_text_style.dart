d451bc0880503e8a94d643d3a12c09785c283d85
Amalia Swastika Indra Prasetya <102946499+AmaliaSwastika@users.noreply.github.com>
2025-08-19 14:26:22 +0700

    Add Theme Text styke

---
+++ lib/theme/theme_text_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:panda_biru/theme/theme_color.dart';
+
+class ThemeTextStyle {
+  TextStyle splash  = GoogleFonts.firaSans(
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+    color: ThemeColor().whiteColor,
+  );
+}
