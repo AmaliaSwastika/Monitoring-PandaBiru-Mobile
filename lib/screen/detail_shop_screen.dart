8d42dac4af1bd1d38c272f8aa3f2d126cf723ed7
Amalia Swastika Indra Prasetya <102946499+AmaliaSwastika@users.noreply.github.com>
2025-08-19 15:42:45 +0700

    Update UI Detail Shop Screen

--- lib/screen/detail_shop_screen.dart
+++ lib/screen/detail_shop_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:panda_biru/model/store_model.dart';
 import 'package:panda_biru/screen/product_shop_screen.dart';
 import 'package:panda_biru/screen/promo_shop_screen.dart';
+import 'package:panda_biru/theme/theme_color.dart';
+import 'package:panda_biru/theme/theme_text_style.dart';
 
 class DetailShopScreen extends StatefulWidget {
   final StoreModel store;
@@ -25,7 +27,18 @@ class _DetailShopScreenState extends State<DetailShopScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.store.name)),
+      backgroundColor: ThemeColor().whiteColor,
+      appBar: AppBar(
+        // title: Text(widget.store.name,
+        //           style: ThemeTextStyle().appBar,
+        //         ),
+        backgroundColor: ThemeColor().blueColor,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: ThemeColor().whiteColor),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+    
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
@@ -33,51 +46,60 @@ class _DetailShopScreenState extends State<DetailShopScreen> {
           children: [
             Text(
               widget.store.name,
-              style: const TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
+              style:ThemeTextStyle().storeName
             ),
             const SizedBox(height: 12),
-            Text("Kode Toko: ${widget.store.code}", style: const TextStyle(fontSize: 16)),
+            Text("Kode Toko: ${widget.store.code}", style:ThemeTextStyle().storeDetail),
             const SizedBox(height: 8),
-            Text("Alamat: ${widget.store.address}", style: const TextStyle(fontSize: 16)),
+            Text("Alamat: ${widget.store.address}", style:ThemeTextStyle().storeDetail),
             const SizedBox(height: 24),
 
             // Tombol Product
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => ProductShopScreen(store: widget.store),
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.shopping_bag),
-                label: const Text("Product"),
-              ),
-            ),
-            const SizedBox(height: 12),
+           // Tombol Product
+SizedBox(
+  width: double.infinity,
+  child: ElevatedButton.icon(
+    onPressed: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => ProductShopScreen(store: widget.store),
+        ),
+      );
+    },
+    icon: Icon(Icons.shopping_bag, color: ThemeColor().whiteColor),
+    label: Text("Product", style:ThemeTextStyle().attendance),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: ThemeColor().blueColor,
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    ),
+  ),
+),
+const SizedBox(height: 12),
 
-            // Tombol Promo
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => PromoShopScreen(store: widget.store),
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.local_offer),
-                label: const Text("Promo"),
-              ),
-            ),
+// Tombol Promo
+SizedBox(
+  width: double.infinity,
+  child: ElevatedButton.icon(
+    onPressed: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => PromoShopScreen(store: widget.store),
+        ),
+      );
+    },
+    icon: Icon(Icons.local_offer, color: ThemeColor().whiteColor),
+    label: Text("Promo", style:ThemeTextStyle().attendance),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: ThemeColor().blueColor,
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    ),
+  ),
+),
+
           ],
         ),
       ),
